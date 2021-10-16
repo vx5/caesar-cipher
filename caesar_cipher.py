bc5006a6cc561a1ffb1801ad65fd7f54833875ba
The User <actualuser@Varuns-MacBook-Pro.local>
2021-10-15 21:18:51 -0700

    trimmed out alphabet

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,26 +1,41 @@
 # Constants used for both encryption and decryption
 minVal = ord('a')
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
+# INPUTS:
+# - Text to be encrypted (str) [text]
+# - Encryption key (int) [key]
+# OUTPUT:
+# - Encrypted text (str)
 def caesarEncrypt(text, key):
 	# Warns when key is 0, because no change will be made to text
 	if not key: return '0_key_error'
 	lowerText = text.lower()
 	output = []
+	# Iterates through given text
 	for char in lowerText:
-		if char not in alphabet: continue
+		# Ignores non-alpha characters
+		if not char.isalpha(): continue
+		# Shifts letters up alphabet by <key> chars
 		adjNum = ord(char) - minVal
 		encrNum = (adjNum + key) % len(alphabet)
 		encrChar = chr(encrNum + minVal)
 		output.append(encrChar)
 	return ''.join(output)
 
+# INPUTS:
+# - Text to be encrypted (str) [text]
+# - Encryption key (int) [key]
+# OUTPUT:
+# - Decrypted text (str)
 def caesarDecrypt(text, key):
 	lowerText = text.lower()
 	output = []
 	for char in lowerText:
-		if char not in alphabet: decrChar = char
+		# Non-alpha characters, if found, are not decrypted
+		if not char.isalpha(): decrChar = char
 		else:
+			# Shifts characters down alphabet by <key> chars
+			# to reverse encryption
 			adjNum = ord(char) - minVal
 			decrNum = (adjNum - key) % len(alphabet)
 			decrChar = chr(decrNum + minVal)
