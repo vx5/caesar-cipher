82e2d0dadbdb1e3df3f92bd5279a03a3a7cfe2f1
Varun Narayan <vx5git@gmail.com>
2022-10-30 19:41:59 -0400

    update to snake_case

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,26 +1,26 @@
 # Constants used for both encryption and decryption
-minVal = ord('a')
-numLetters = 26
+min_val = ord('a')
+num_letters = 26
 
 # INPUTS:
 # - Text to be encrypted (str) [text]
 # - Encryption key (int) [key]
 # OUTPUT:
 # - Encrypted text (str)
-def caesarEncrypt(text, key):
+def caesar_encrypt(text, key):
 	# Warns when key is 0, because no change will be made to text
 	if not key: return '0_key_error'
-	lowerText = text.lower()
+	lower_text = text.lower()
 	output = []
 	# Iterates through given text
-	for char in lowerText:
+	for char in lower_text:
 		# Ignores non-alpha characters
 		if not char.isalpha(): continue
 		# Shifts letters up alphabet by <key> chars
-		adjNum = ord(char) - minVal
-		encrNum = (adjNum + key) % numLetters
-		encrChar = chr(encrNum + minVal)
-		output.append(encrChar)
+		adj_num = ord(char) - min_val
+		encr_num = (adj_num + key) % num_letters
+		encr_char = chr(encr_num + min_val)
+		output.append(encr_char)
 	return ''.join(output)
 
 # INPUTS:
@@ -28,17 +28,17 @@ def caesarEncrypt(text, key):
 # - Encryption key (int) [key]
 # OUTPUT:
 # - Decrypted text (str)
-def caesarDecrypt(text, key):
-	lowerText = text.lower()
+def caesar_decrypt(text, key):
+	lower_text = text.lower()
 	output = []
-	for char in lowerText:
+	for char in lower_text:
 		# Non-alpha characters, if found, are not decrypted
-		if not char.isalpha(): decrChar = char
+		if not char.isalpha(): decr_char = char
 		else:
 			# Shifts characters down alphabet by <key> chars
 			# to reverse encryption
-			adjNum = ord(char) - minVal
-			decrNum = (adjNum - key) % numLetters
-			decrChar = chr(decrNum + minVal)
-		output.append(decrChar)
+			adj_num = ord(char) - min_val
+			decr_num = (adj_num - key) % num_letters
+			decr_char = chr(decr_num + min_val)
+		output.append(decr_char)
 	return ''.join(output)
